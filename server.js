8239429d8d8230f3e7041120b05b2afb82d1fc36
ericus-star <your-email@example.com>
2026-07-27 15:31:03 +0300

    Add SEO meta tags, sitemap, and robots.txt routes

--- server.js
+++ server.js
@@ -52,6 +52,38 @@ const requireAuth = (req, res, next) => {
   res.redirect('/login');
 };
 
+// --- SEO & INDEXING ROUTES ---
+
+// Dynamic Sitemap for Google Search Console
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://erivox.onrender.com/</loc>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>https://erivox.onrender.com/movies</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
+    </url>
+    <url>
+      <loc>https://erivox.onrender.com/tv</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`;
+  res.send(sitemapXml);
+});
+
+// Serve Robots.txt directly
+app.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.send("User-agent: *\nAllow: /\nDisallow: /admin\nDisallow: /login\n\nSitemap: https://erivox.onrender.com/sitemap.xml");
+});
+
 // --- PUBLIC ROUTES ---
 
 app.get('/', async (req, res) => {
